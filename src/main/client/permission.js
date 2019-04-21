6533fa72e320c39fd0b3c3d8c06317b3bb6e33b8
setvect <setvect@naver.com>
2019-04-21 11:38:40 +0900

    웹 템플릿 적용 - 작업중

--- src/main/client/permission.js
+++ src/main/client/permission.js
@@ -1,22 +1,16 @@
 import router from './router'
 import store from './store'
-import NProgress from 'nprogress' // progress bar
-import 'nprogress/nprogress.css' // progress bar style
 import { Message } from 'element-ui'
 import { getToken } from '@/utils/auth' // getToken from cookie
 
-NProgress.configure({ showSpinner: false })// NProgress configuration
-
-const whiteList = ['/login'] // 不重定向白名单
+const whiteList = ['/login'] // 로그인 안해도 접근 가능한 페이지
 router.beforeEach((to, from, next) => {
-  NProgress.start()
   if (getToken()) {
     if (to.path === '/login') {
       next({ path: '/' })
-      NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
     } else {
       if (store.getters.roles.length === 0) {
-        store.dispatch('GetInfo').then(res => { // 拉取用户信息
+        store.dispatch('GetInfo').then(res => { // 사용자 정보 가져옴
           next()
         }).catch((err) => {
           store.dispatch('FedLogOut').then(() => {
@@ -32,12 +26,7 @@ router.beforeEach((to, from, next) => {
     if (whiteList.indexOf(to.path) !== -1) {
       next()
     } else {
-      next(`/login?redirect=${to.path}`) // 否则全部重定向到登录页
-      NProgress.done()
+      next(`/login?redirect=${to.path}`) // 로그인페이지로 리다리렉션
     }
   }
-})
-
-router.afterEach(() => {
-  NProgress.done() // 结束Progress
 })
--- webpack/webpack.dev.conf.js
+++ webpack/webpack.dev.conf.js
@@ -31,7 +31,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     compress: true,
     host: HOST || config.dev.host,
     port: PORT || config.dev.port,
-    open: config.dev.autoOpenBrowser,
+    // open: config.dev.autoOpenBrowser,
     overlay: config.dev.errorOverlay
       ? { warnings: false, errors: true }
       : false,
