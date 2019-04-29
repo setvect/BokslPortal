fb2cb7326d6e3750ee46edb01db976b18aa07de8
setvect <setvect@naver.com>
2019-04-29 22:16:36 +0900

    웹 템플릿 적용 - 홈피 틀

--- src/main/client/views/home/main.vue
+++ src/main/client/views/home/main.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="app-container">홈피 > 홈</div>
+  <el-container >
+    <el-aside width="250px" style="background-color: rgb(238, 241, 246)">
+      <el-menu :default-openeds="['1', '3']">
+        <el-submenu index="3">
+          <template slot="title">
+            <i class="el-icon-setting"></i>Navigator Three
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="3-1">Option 1</el-menu-item>
+            <el-menu-item index="3-2">Option 2</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+
+    <el-container>
+      <h1>홈피</h1>
+    </el-container>
+  </el-container>
 </template>
+
+<style>
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    color: #333;
+  }
+</style>
 
 <script>
 export default {
   data() {
+    const item = {
+      date: '2016-05-02',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles'
+    };
     return {
+      tableData: Array(20).fill(item)
     }
-  },
-  methods: {
   }
-}
+};
 </script>
-
-<style scoped>
-</style>
-
