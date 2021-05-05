1bf3e510fa5511c84235627ee6f43fd0d80a4f37
setvect <setvect@naver.com>
2021-05-05 23:33:22 +0900

    복슬 노트 마크다운 문서 편집 기능 추가 - 작업중

--- src/main/client/app/note/noteAdd.vue
+++ src/main/client/app/note/noteAdd.vue
@@ -104,37 +104,10 @@ import 'codemirror/lib/codemirror.css'
 // theme css
 import 'codemirror/theme/eclipse.css'
 import 'codemirror/theme/night.css'
-// language
-import 'codemirror/mode/vue/vue.js'
 // active-line.js
 import 'codemirror/addon/selection/active-line.js'
-// styleSelectedText
-import 'codemirror/addon/selection/mark-selection.js'
-import 'codemirror/addon/search/searchcursor.js'
-// highlightSelectionMatches
-import 'codemirror/addon/scroll/annotatescrollbar.js'
-import 'codemirror/addon/search/matchesonscrollbar.js'
-import 'codemirror/addon/search/searchcursor.js'
-import 'codemirror/addon/search/match-highlighter.js'
-// keyMap
-import 'codemirror/mode/clike/clike.js'
-import 'codemirror/addon/edit/matchbrackets.js'
-import 'codemirror/addon/comment/comment.js'
-import 'codemirror/addon/dialog/dialog.js'
-import 'codemirror/addon/dialog/dialog.css'
-import 'codemirror/addon/search/searchcursor.js'
-import 'codemirror/addon/search/search.js'
-import 'codemirror/keymap/sublime.js'
-// foldGutter
-import 'codemirror/addon/fold/foldgutter.css'
-import 'codemirror/addon/fold/brace-fold.js'
-import 'codemirror/addon/fold/comment-fold.js'
-import 'codemirror/addon/fold/foldcode.js'
-import 'codemirror/addon/fold/foldgutter.js'
-import 'codemirror/addon/fold/indent-fold.js'
-import 'codemirror/addon/fold/markdown-fold.js'
-import 'codemirror/addon/fold/xml-fold.js'
 
+import 'codemirror/addon/edit/continuelist.js'
 import 'codemirror/mode/markdown/markdown.js'
 
 const DEFAULT_AUTO_SAVE_TIME = 15;
@@ -167,20 +140,11 @@ export default {
       markdown: false,
       cmOption: {
         tabSize: 2,
-        styleActiveLine: true,
         lineNumbers: true,
-        line: true,
-        foldGutter: true,
-        styleSelectedText: true,
-        mode: 'text/x-markdown',
-        // keyMap: "sublime",
-        matchBrackets: true,
-        theme: "night",
-        showCursorWhenSelecting: true,
-        extraKeys: { "Ctrl": "autocomplete" },
-        hintOptions: {
-          completeSingle: false
-        }
+        styleActiveLine: true,
+        mode: 'markdown',
+        theme: "eclipse",
+        extraKeys: {"Enter": "newlineAndIndentContinueMarkdownList"}
       },
     };
   },
